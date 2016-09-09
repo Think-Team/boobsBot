f8b7b5a96740590ce871d0210a557991dcb7264b
siktirmirza <alicowman79@gmail.com>
2016-09-09 18:42:04 +0430

    Added /butts feature :/\/

--- boobs.py
+++ boobs.py
@@ -15,8 +15,8 @@ reload(sys)
 from telebot import types
 sys.setdefaultencoding("utf-8")
 
-bot = telebot.TeleBot('The_Token')
-#By #SikTirMirza
+bot = telebot.TeleBot('276815499:AAEDX6l__u4iObZE1BFBGSq1oOJNEOpbPBU')
+
 @bot.message_handler(func=lambda message: True)
 def m(m):
     if m.text == '/boobs':
@@ -29,6 +29,18 @@ def m(m):
         rrrrr = "http://media.oboobs.ru/{}".format(rrrr)
         urllib.urlretrieve (rrrrr, "boobs.jpg")
         photo = open('boobs.jpg', 'rb')
+        bot.send_photo(cid, photo)
+        bot.send_photo(cid, "FILEID")
+    if m.text == '/butts':
+        cid = m.chat.id
+        req = urllib2.Request("http://api.obutts.ru/noise/1")
+        opener = urllib2.build_opener()
+        f = opener.open(req)
+        parsed_json = json.loads(f.read())
+        rrrr = parsed_json[0]['preview']
+        rrrrr = "http://media.obutts.ru/{}".format(rrrr)
+        urllib.urlretrieve (rrrrr, "butts.jpg")
+        photo = open('butts.jpg', 'rb')
         bot.send_photo(cid, photo)
         bot.send_photo(cid, "FILEID")
     if m.text == '/leave':
