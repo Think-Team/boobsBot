012cb266e1b700c38bf6be21932d8b25cc028d97
Ali <alicowman79@gmail.com>
2021-07-02 20:59:40 +0430

    Update boobs.py

--- boobs.py
+++ boobs.py
@@ -48,19 +48,19 @@ def m(m):
         print 'command leave'
         print '{}'.format(m.from_user.first_name)
         print '{}'.format(m.from_user.username)
-@bot.message_handler(commands=['boobs'])
-def handler(m):
-    cid = m.chat.id
-    req = urllib2.Request("http://api.oboobs.ru/noise/1")
-    opener = urllib2.build_opener()
-    f = opener.open(req)
-    parsed_json = json.loads(f.read())
-    rrrr = parsed_json[0]['preview']
-    rrrrr = "http://media.oboobs.ru/{}".format(rrrr)
-    urllib.urlretrieve (rrrrr, "boobs.jpg")
-    photo = open('boobs.jpg', 'rb')
-    bot.send_photo(cid, photo)
-    bot.send_photo(cid, "FILEID")
-
+#@bot.message_handler(commands=['boobs'])
+#def handler(m):
+#    cid = m.chat.id
+#    req = urllib2.Request("http://api.oboobs.ru/noise/1")
+#    opener = urllib2.build_opener()
+#    f = opener.open(req)
+#    parsed_json = json.loads(f.read())
+#    rrrr = parsed_json[0]['preview']
+#    rrrrr = "http://media.oboobs.ru/{}".format(rrrr)
+#    urllib.urlretrieve (rrrrr, "boobs.jpg")
+#    photo = open('boobs.jpg', 'rb')
+#    bot.send_photo(cid, photo)
+#    bot.send_photo(cid, "FILEID")
+#
 
 bot.polling(none_stop=True, timeout=20)
